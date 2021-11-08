677306bf51166e0e62d269ad2289c6e4af011e98
Eugen <krizhik@gmail.com>
2021-11-08 10:40:10 +0300

    first commit

---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            {/*hrouter, provider*/}
+            <>
+                {/*<Header/>*/}
+                {/*<Main/>*/}
+            </>
+        </div>
+    )
+};
+
+export default App;
